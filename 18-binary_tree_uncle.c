76f5f97e81c74a268bdab9e4c3f87daa973fadda
Castro Hamiltone <castromiltone@gmail.com>
2023-08-04 02:04:09 +0300

    fix

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
-	if (node->parent->parent->left == node->parent)
+	if (node->parent->parent->right == node->parent)
 		return (node->parent->parent->left);
 	
 	return (node->parent->parent->right);
