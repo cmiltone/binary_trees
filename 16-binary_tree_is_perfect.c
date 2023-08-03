2a0149662196c25c2a75fe1968ae473a86079610
Castro Hamiltone <castromiltone@gmail.com>
2023-08-04 01:43:07 +0300

    16-binary_tree_is_perfect.c

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - checks if tree is perfect
+ * @tree: pointer to root node
+ * Return: int
+*/
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{}
